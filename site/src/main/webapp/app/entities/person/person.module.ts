6a8b571fc227d056c75ba93510ea9b2d3d8e2d46
Anthony Leon <anthonyfabian22ll@gmail.com>
2022-10-20 17:50:45 +0200

    Generate person-card.component

--- site/src/main/webapp/app/entities/person/person.module.ts
+++ site/src/main/webapp/app/entities/person/person.module.ts
@@ -5,9 +5,10 @@ import { PersonDetailComponent } from './detail/person-detail.component';
 import { PersonUpdateComponent } from './update/person-update.component';
 import { PersonDeleteDialogComponent } from './delete/person-delete-dialog.component';
 import { PersonRoutingModule } from './route/person-routing.module';
+import { PersonCardComponent } from './person-card/person-card.component';
 
 @NgModule({
   imports: [SharedModule, PersonRoutingModule],
-  declarations: [PersonComponent, PersonDetailComponent, PersonUpdateComponent, PersonDeleteDialogComponent],
+  declarations: [PersonComponent, PersonDetailComponent, PersonUpdateComponent, PersonDeleteDialogComponent, PersonCardComponent],
 })
 export class PersonModule {}
