d972645b8c6e87c8a9f2c72b69b3a9cea4295527
Anthony Leon <anthonyfabian22ll@gmail.com>
2022-10-20 13:32:56 +0200

    Solve person mapper

--- api/src/main/java/com/anthonylldev/streaming/service/mapper/PersonMapper.java
+++ api/src/main/java/com/anthonylldev/streaming/service/mapper/PersonMapper.java
@@ -8,4 +8,17 @@ import org.mapstruct.*;
  * Mapper for the entity {@link Person} and its DTO {@link PersonDTO}.
  */
 @Mapper(componentModel = "spring")
-public interface PersonMapper extends EntityMapper<PersonDTO, Person> {}
+public interface PersonMapper extends EntityMapper<PersonDTO, Person> {
+
+    @Override
+    @Mapping(target = "films", ignore = true)
+    @Mapping(target = "removeFilm", ignore = true)
+    Person toEntity(PersonDTO dto);
+
+    @Override
+    @Named("partialUpdate")
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "films", ignore = true)
+    @Mapping(target = "removeFilm", ignore = true)
+    void partialUpdate(@MappingTarget Person entity, PersonDTO dto);
+}
