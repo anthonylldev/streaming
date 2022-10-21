544d29353e92ded0e6c44e99c63dc2abfd6b7655
Anthony Leon <anthonyfabian22ll@gmail.com>
2022-10-21 14:02:26 +0200

    Add crud table

--- site/src/main/webapp/app/entities/person/person.module.ts
+++ site/src/main/webapp/app/entities/person/person.module.ts
@@ -6,9 +6,13 @@ import { PersonUpdateComponent } from './update/person-update.component';
 import { PersonDeleteDialogComponent } from './delete/person-delete-dialog.component';
 import { PersonRoutingModule } from './route/person-routing.module';
 import { PersonCardComponent } from './person-card/person-card.component';
+import { TableModule } from 'primeng/table';
+import { ToolbarModule } from 'primeng/toolbar';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
 
 @NgModule({
-  imports: [SharedModule, PersonRoutingModule],
+  imports: [SharedModule, PersonRoutingModule, TableModule, ToolbarModule, ButtonModule, InputTextModule],
   declarations: [PersonComponent, PersonDetailComponent, PersonUpdateComponent, PersonDeleteDialogComponent, PersonCardComponent],
 })
 export class PersonModule {}
